44c6749da61925ae078c24de9e9a7bc681d47365
Tushar Patil <tusharpatil17012001@gmail.com>
2025-10-24 09:53:45 +0530

    Initial commit

---
+++ server/configs/imageKit.js
@@ -0,0 +1,10 @@
+import ImageKit from '@imagekit/nodejs';
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const imageKit = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
+});
+
+export default imageKit;
