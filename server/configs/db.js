c85802c548375ce61b927c0a14b762c3bb25494f
Tushar Patil <tusharpatil17012001@gmail.com>
2025-10-26 12:10:10 +0530

    Updated backend logic and environment settings

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,26 +1,25 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        mongoose.connection.on("connected", ()=>{console.log("Database connected successfully")})
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("✅ Database connected successfully");
+    });
 
-        let mongodbURI = process.env.MONGODB_URI;
-        const projectName = 'resume-builder';
+    const mongodbURI = process.env.MONGODB_URI;
 
-        if(!mongodbURI){
-            throw new Error("MONGODB_URI environment variable not set")
-        }
-
-        if(mongodbURI.endsWith('/')){
-            mongodbURI = mongodbURI.slice(0,-1)
-        }
-
-        await mongoose.connect(`${mongodbURI}/${projectName}`)
-
-    } catch (error) {
-        console.error("Error connection to MongoDB:", error)
-
+    if (!mongodbURI) {
+      throw new Error("MONGODB_URI environment variable not set");
     }
-}
+
+    await mongoose.connect(mongodbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.error("❌ Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
